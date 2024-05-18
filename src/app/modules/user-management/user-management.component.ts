aead4b3e08999fff93e5e77a480b1fa0a4a564c7
Purple Pote <viisael97@hotmail.com>
2024-05-18 13:00:09 -0300

    Change: Divisão do componente em sub-componentes. e mudanças no css.

--- src/app/modules/user-management/user-management.component.ts
+++ src/app/modules/user-management/user-management.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../../components/button/button.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { UserTableComponent } from '../../components/table-user-data/user-table/user-table.component';
 
 @Component({
   selector: 'app-user-management',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [UserTableComponent],
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.scss'
 })
