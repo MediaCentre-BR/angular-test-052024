cff08d8626bcaecd7b041be3b0b2ac99e7aeea9d
Purple Pote <viisael97@hotmail.com>
2024-05-19 13:03:29 -0300

    Add: Funcionalidades como Search e Delete.

--- src/app/modules/user-management/user-management.component.ts
+++ src/app/modules/user-management/user-management.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { UserTableComponent } from '../../components/table-user-data/user-table/user-table.component';
+import { Component, inject } from '@angular/core';
+import { UserTableComponent } from '../../components/user-table/user-table.component';
+import { User } from '../../models/user.interface';
+import { DataService } from '../../services/data/data.service';
 
 @Component({
   selector: 'app-user-management',
@@ -11,4 +12,59 @@ import { UserTableComponent } from '../../components/table-user-data/user-table/
 })
 export class UserManagementComponent {
 
+  dataService: DataService = inject(DataService);
+
+  userList: User[] = [];
+  filteredUsersList: User[] = [];
+
+
+
+  /**
+   * Busca os dados na API e salva em userList e filteredUsersList.
+   */
+  constructor()
+  {
+    this.dataService.getAllUsers().then((userDataList:User[]) => {
+      this.userList = userDataList;
+      this.filteredUsersList = userDataList;
+    })
+
+  }
+
+  
+  /**
+   * [NÃO ESTÁ COMPLETO] Deleta um usário com base no parâmetro userId. 
+   * @param userId - ID do usuário de interesse
+   */
+  deleteUser(userId: number) {
+    let newUserList = this.userList.filter(userData => userData.id !== userId);
+
+    console.log('a');
+
+    this.filteredUsersList = newUserList;
+    this.userList = newUserList;
+  }
+
+
+  /** 
+   * Filtra os usuários por username de acordo com o parâmetro 
+   * fornecido e salva o resultado em UserManagementComponent.filteredUsersList
+   * 
+   * É chamado a cada input do usuário.
+   * 
+   * @param text - Parâmetro de filtragem. 
+  */
+  onInputFilterResults(event: any): void 
+  {
+    let text:string = event.target.value;
+
+    if (!text) {
+      this.filteredUsersList = this.userList;
+      return;
+    }
+
+    this.filteredUsersList = this.userList.filter(
+      user => user?.username.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
